e176df388f709ed817ab603b5bb4c5e9c6942558
krititwa <krititwaray97@gmail.com>
2026-04-01 22:31:40 +0530

    5th

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require("express");
+const express  require("express");
 const app = express();
 
 
 
-app.lis
-ten(3000, () => {
+app.listen(3000, () => {
     console.log(`Server is running on port: 3000`)
 })
